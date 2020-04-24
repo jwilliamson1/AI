c2a7cebef96b730b3f4c101e589a21185ac2324e
Joseph Williamson <jwilliamson@kbra.com>
2020-04-23 23:47:06 -0400

    get diagonal up to right working

--- lab3/lab3.py
+++ lab3/lab3.py
@@ -89,48 +89,87 @@ def onEdge(listOfTuples):
     return tup[0] == 0 or tup[1] == 0 or tup[1] == 6
 
 def scoreBoard(board, currentPlayerId, otherPlayerId):
-  cpLongest = board.longest_chain(currentPlayerId)
-  opLongest = board.longest_chain(otherPlayerId)
+    cpLongest = board.longest_chain(currentPlayerId)
+    opLongest = board.longest_chain(otherPlayerId)
 
-  score = 0
+    score = 10 * cpLongest
   
-  cpChains = board.chain_cells(currentPlayerId)
-  opChains = board.chain_cells(otherPlayerId)
-    
-  if cpLongest == 3 or opLongest == 3:
+    cpChains = filter(lambda x: x > 2, board.chain_cells(currentPlayerId))
+    opChains = filter(lambda x: x > 2, board.chain_cells(otherPlayerId))
 
     for col in range(7):
-    
-      if board.get_height_of_column(col) > 0:
 
-        tryWinBoard = board.do_move(col)
+        if board.get_height_of_column(col) > 0:
 
-        newChains = tryWinBoard.chain_cells(currentPlayerId)
+            tryWinBoard = board.do_move(col)
 
-        for nc in newChains:
-          len1 = len(nc)
+            newChains = tryWinBoard.chain_cells(currentPlayerId)
+            otherPlayerNewChains = tryWinBoard.chain_cells(otherPlayerId)
 
-          score += 100 * len1
+            for otherPlayerChain in otherPlayerNewChains:
+                chainLength = len(otherPlayerChain)
+                # only bother with chains bigger than 0
+                if chainLength > 1:
+                    # get ends
+                    endX = otherPlayerChain[0][0]
+                    endY = otherPlayerChain[0][1]
+                    startX = otherPlayerChain[chainLength-1][0]
+                    startY = otherPlayerChain[chainLength-1][1]
 
-          if len(nc) == 4:
+                    if startX == endX:
+                        if startY > 0:
+                            startAdjacent = board.get_cell(startX, startY-1)
+                            #check if opponent is next to end
+                            if startAdjacent == currentPlayerId:
+                                score += 10 * chainLength
+                        if endY < 6:
+                            endAdjacent = board.get_cell(endX, endY+1)
+                            if  endAdjacent == currentPlayerId:
+                                score += 10 * chainLength
 
-            score += 5000
+                    if startY == endY:
+                        if startX > 0:
+                            startAdjacent = board.get_cell(startX-1, startY)
+                            #check if opponent is next to end
+                            if startAdjacent == currentPlayerId:
+                                score += 10 * chainLength
+                        # if endX < 5:
+                        #     endAdjacent = board.get_cell(endX+1, endY)
+                        #     if  endAdjacent == currentPlayerId:
+                        #         score += 10
+                    if startX > endX:
+                        if endY < 6 and endX < 5:
+                            startAdjacent = board.get_cell(endX-1, endY+1)
+                            if startAdjacent == currentPlayerId:
+                                score += 10 * chainLength
+
+            for nc in newChains:
+                
+                len1 = len(nc)
+
+                if len1 > 1:
+
+                    score += 100 * len1
+
+                    if len(nc) == 4:
+
+                        score += 5000
 
   # Prefer having your pieces in the center of the board.
   # TODO: handle gaps in long chain, and do we need more pieces vertically to get there?      
   # TODO: do we care what the oponent is doing or does alphabeta cover that?
-  for row in range(6):      
-      for col in range(7):
-              #subtract more from current player if farther from middle
-          if board.get_cell(row, col) == currentPlayerId:
-              score -= abs(3-col)
-              score -= abs(3-row)
-              #add more to current player if opponent is farther from middle
-          elif board.get_cell(row, col) == otherPlayerId:
-              score += abs(3-col)
-              score += abs(3-row)
+    for row in range(6):      
+        for col in range(7):
+                #subtract more from current player if farther from middle
+            if board.get_cell(row, col) == currentPlayerId:
+                score -= abs(3-col)
+                score -= abs(3-row)
+                #add more to current player if opponent is farther from middle
+            elif board.get_cell(row, col) == otherPlayerId:
+                score += abs(3-col)
+                score += abs(3-row)
 
-  return score
+    return score
 
 
 ## Create a "player" function that uses the focused_evaluate function
